362aff54853bebe91e35f161651ebd7aa7cff767
Youri van der Lans <youri@itflows.nl>
2011-09-29 23:44:28 +0200

    Added beforeRender callback to appController and added a function for getting the menu (pages).

--- app/app_controller.php
+++ app/app_controller.php
@@ -2,4 +2,26 @@
 class AppController extends Controller
 {
 	var $helpers = array('AssetCompress.AssetCompress');
+	
+	function beforeRender()
+	{
+		if ( !isset($this->params['prefix']) )
+		{
+			$pages = $this->getMenu();
+			$staticActions = $this->Page->staticActions;
+			
+			$this->set(compact('pages', 'staticActions'));
+		}
+	}
+	
+	function getMenu()
+	{
+		$this->loadModel('Page');
+		
+		$pages = $this->Page->find('all', array(
+			'order' => 'position'
+		));
+		
+		return $pages;
+	}
 }
