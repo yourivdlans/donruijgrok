8afa2428f7c1c66501a6137f93c61dd495c9cbf0
Youri van der Lans <youri@itflows.nl>
2011-10-03 16:57:34 +0200

    Set title for layout on app controller and always set to view.

--- app/app_controller.php
+++ app/app_controller.php
@@ -2,15 +2,24 @@
 class AppController extends Controller
 {
 	var $helpers = array('AssetCompress.AssetCompress');
+	var $title_for_layout = 'Don Ruijgrok';
 	
 	function beforeRender()
 	{
+		// Front-end
 		if ( !isset($this->params['prefix']) )
 		{
+			$title_for_layout = $this->title_for_layout;
+			
 			$pages = $this->getMenu();
 			$staticActions = $this->Page->staticActions;
 			
-			$this->set(compact('pages', 'staticActions'));
+			$this->set(compact('title_for_layout', 'pages', 'staticActions'));
+		}
+		// Back-end
+		else
+		{
+			
 		}
 	}
 	
