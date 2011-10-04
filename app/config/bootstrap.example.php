f34879cfee8d3174bf94256fc7df7483a81ef988
Youri van der Lans <youri@itflows.nl>
2011-10-04 13:51:58 +0200

    Removed bootstrap example from ignore file.

---
+++ app/config/bootstrap.example.php
@@ -0,0 +1,14 @@
+<?php
+// Windows
+define('LOCALE_NL', 'nld_nld');
+define('LOCALE_EN', 'us_us');
+// Unix
+//define('LOCALE_NL', 'nl_NL.utf8');
+//define('LOCALE_EN', 'en_US.utf8');
+
+// OSX
+//define('LOCALE_NL', 'nl_NL.UTF-8');
+//define('LOCALE_EN', 'en_US.UTF-8');
+
+setlocale(LC_ALL, LOCALE_NL);
+date_default_timezone_set('Europe/Amsterdam');
