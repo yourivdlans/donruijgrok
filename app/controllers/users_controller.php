12b81386f12fd8455ace49f214b728150add5a3a
Youri van der Lans <youri@itflows.nl>
2011-10-27 16:09:38 +0200

    Added beforeFilter to users controller so it can set the layout to login when needed. Redirected user after login.

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -3,9 +3,22 @@ class UsersController extends AppController
 {
 	var $name = 'Users';
 	
+	function beforeRender()
+	{
+		parent::beforeRender();
+		
+		if ( $this->params['action']['admin_login'] )
+		{
+			$this->layout = 'login';
+		}
+	}
+	
 	function admin_login()
 	{
-		$this->layout = 'login';
+		if ($this->Auth->user())
+		{
+			$this->redirect($this->Auth->redirect());
+		}
 	}
 	
 	function logout()
