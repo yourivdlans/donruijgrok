9ffb05d2dd6ac80bfaa20a546861a54bc8b30474
Youri van der Lans <youri@itflows.nl>
2011-11-07 15:57:18 +0100

    Set validation error message for picture title.

--- app/models/picture.php
+++ app/models/picture.php
@@ -6,11 +6,7 @@ class Picture extends AppModel {
 		'title' => array(
 			'notempty' => array(
 				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+				'message' => 'Can not be empty'
 			),
 		)
 	);
